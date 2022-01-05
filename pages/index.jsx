3dc1842b65b9edaf6f3c1f10cdd4fa2369fd4281
fadumoaideed <fadumo.ahmed.aideed@gmail.com>
2022-01-05 21:35:59 +0000

    theme config update

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react'
 import Head from 'next/head'
 import { keyframes } from '@emotion/react'
 import styled from '@emotion/styled'
-import { slideInRight } from 'react-animations'
+import { slideInLeft, slideInRight } from 'react-animations'
 import {
    Box,
    Grid,
@@ -14,7 +14,12 @@ import {
    Text
 } from '@chakra-ui/react'
 import Carousel from '../components/Carousel/Carousel'
+
 let prefix = '/westcic-next'
+
+const SlideInLeft = styled.div`
+   animation: 2s ${keyframes`${slideInLeft}`};
+`
 const SlideInRight = styled.div`
    animation: 2s ${keyframes`${slideInRight}`};
 `
@@ -29,50 +34,39 @@ export default function Home() {
          <Box className="home-container">
             <Carousel />
 
-            <SimpleGrid columns={[1, 2, 2]} pb="4">
-               <SlideInRight>
-                  <Box bg="grey" height="400px" align="center" className="box">
-                     <Stack
-                        spacing={3}
-                        p="5"
-                        display="flex"
+            <SimpleGrid columns={[1, 1, 2]}>
+               <SlideInLeft>
+                  <Box
+                     bg="teal.500"
+                     height="400px"
+                     // pt="50px"
+                     width={['100%', '100%', '50vw', '50vw']}
+                     className="box"
+                  >
+                     <Heading
+                        fontSize={['2xl', '2xl', '5xl', '6xl']}
                         align="center"
-                     ></Stack>
+                     >
+                        Sustainable Innovation
+                     </Heading>
+                     <Text
+                        fontSize={['xsm', '2md', '5md', '6md']}
+                        variant="outline"
+                        align="justify"
+                        fontWeight="thin"
+                        pt="30px"
+                        px={['30px', '60px', '85px', '200px']}
+                     >
+                        Lorem Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex
+                        ea commodo consequat.
+                     </Text>
                   </Box>
-               </SlideInRight>
-
-               <Box bg="teal.500" height="400px" align="center">
-                  <Stack
-                     spacing={3}
-                     display="flex"
-                     align="center"
-                     p="5"
-                  ></Stack>
-               </Box>
+               </SlideInLeft>
             </SimpleGrid>
          </Box>
       </Fragment>
    )
-}
-
-{
-   /* <Heading
-fontSize={['xl', '2xl', '5xl', '6xl']}
-align="center"
->
-Sustainable Innovation
-</Heading>
-<Text
-fontSize={['2sm', '2md', '5md', '6md']}
-variant="outline"
-align="justify"
-fontWeight="thin"
-p="5"
->
-Lorem Lorem ipsum dolor sit amet, consectetur adipiscing
-elit, sed do eiusmod tempor incididunt ut labore et
-dolore magna aliqua. Ut enim ad minim veniam, quis
-nostrud exercitation ullamco laboris nisi ut aliquip ex
-ea commodo consequat.
-</Text> */
 }
