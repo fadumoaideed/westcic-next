91c07c29b5c2fe314aebafd4af837b16a448a25a
fadumoaideed <71390607+fadumoaideed@users.noreply.github.com>
2022-01-07 16:38:42 +0000

    Homepage (#4)
    * animation implemented with emotion-ui
    
    * theme config update
    
    * fix: typography responsiveness

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,19 @@
 import React, { Fragment } from 'react'
 import Head from 'next/head'
-import {
-   Box,
-   Grid,
-   Flex,
-   GridItem,
-   SimpleGrid,
-   Stack,
-   Heading,
-   Text
-} from '@chakra-ui/react'
+import { keyframes } from '@emotion/react'
+import styled from '@emotion/styled'
+import { slideInLeft, slideInRight } from 'react-animations'
+import { Box, SimpleGrid, Heading, Text, Spacer } from '@chakra-ui/react'
 import Carousel from '../components/Carousel/Carousel'
+
 let prefix = '/westcic-next'
+
+const SlideInLeft = styled.div`
+   animation: 2s ${keyframes`${slideInLeft}`};
+`
+const SlideInRight = styled.div`
+   animation: 2s ${keyframes`${slideInRight}`};
+`
 export default function Home() {
    return (
       <Fragment>
@@ -23,47 +25,75 @@ export default function Home() {
          <Box className="home-container">
             <Carousel />
 
-            <SimpleGrid columns={[1, 2, 2]} pb="4">
-               <Box bg="grey" height="400px" align="center" className="box">
-                  <Stack
-                     spacing={3}
-                     p="5"
-                     display="flex"
-                     align="center"
-                  ></Stack>
+            <SimpleGrid columns={[1, 1, 2]} mx={['0', '0', '100px', '100px']}>
+               <SlideInLeft>
+                  <Box
+                     bg="teal.500"
+                     height="400px"
+                     width={['100%', '100%', '50vw - 110px', '50vw - 110px']}
+                     className="box"
+                  >
+                     <Heading
+                        fontSize={['5xl', '5xl', '5xl', '6xl']}
+                        align="center"
+                     >
+                        Sustainable Innovation
+                     </Heading>
+                     <Text
+                        fontSize={['xsm', '2md', '5md', '6md']}
+                        variant="outline"
+                        align="justify"
+                        fontWeight="thin"
+                        pt="30px"
+                        px={['30px', '30px', '70px', '80px']}
+                     >
+                        Lorem Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex
+                        ea commodo consequat.
+                     </Text>
+                  </Box>
+               </SlideInLeft>
+               <Box bg="grey">
+                  <Text>Something interactive here</Text>
                </Box>
-               <Box bg="tomato" height="400px" align="center">
-                  <Stack
-                     spacing={3}
-                     display="flex"
-                     align="center"
-                     p="5"
-                  ></Stack>
+            </SimpleGrid>
+            <SimpleGrid columns={[1, 1, 2]} mx={['0', '0', '100px', '100px']}>
+               <Box bg="grey">
+                  <Text>Something interactive here</Text>
                </Box>
+               <SlideInRight>
+                  <Box
+                     bg="teal.500"
+                     height="400px"
+                     width={['100%', '100%', '50vw - 100px', '50vw - 100px']}
+                     className="box"
+                  >
+                     <Heading
+                        fontSize={['5xl', '5xl', '5xl', '6xl']}
+                        align="center"
+                     >
+                        Human Centered Design
+                     </Heading>
+                     <Text
+                        fontSize={['xsm', '2md', '5md', '6md']}
+                        variant="outline"
+                        align="justify"
+                        fontWeight="thin"
+                        pt="30px"
+                        px={['30px', '30px', '70px', '80px']}
+                     >
+                        Lorem Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex
+                        ea commodo consequat.
+                     </Text>
+                  </Box>
+               </SlideInRight>
             </SimpleGrid>
          </Box>
       </Fragment>
    )
-}
-
-{
-   /* <Heading
-fontSize={['xl', '2xl', '5xl', '6xl']}
-align="center"
->
-Sustainable Innovation
-</Heading>
-<Text
-fontSize={['2sm', '2md', '5md', '6md']}
-variant="outline"
-align="justify"
-fontWeight="thin"
-p="5"
->
-Lorem Lorem ipsum dolor sit amet, consectetur adipiscing
-elit, sed do eiusmod tempor incididunt ut labore et
-dolore magna aliqua. Ut enim ad minim veniam, quis
-nostrud exercitation ullamco laboris nisi ut aliquip ex
-ea commodo consequat.
-</Text> */
 }
--- src/theme.js
+++ src/theme.js
@@ -2,10 +2,10 @@ import { extendTheme } from '@chakra-ui/react'
 import { createBreakpoints } from '@chakra-ui/theme-tools'
 
 const breakpoints = createBreakpoints({
-   sm: '40em',
-   md: '52em',
-   lg: '64em',
-   xl: '80em'
+   sm: '40em', //640px
+   md: '52em', //800px
+   lg: '64em', //1024px
+   xl: '80em' //1280
 })
 
 // const components = {
@@ -39,6 +39,39 @@ const config = {
    //    body: 'Inter',
    //    heading: 'Inter'
    // },
+   // fonts: {
+   //    body: "system-ui, sans-serif",
+   //    heading: "Georgia, serif",
+   //    mono: "Menlo, monospace",
+   //  },
+   //  fontSizes: {
+   //    xxxs: "0.45rem",
+   //    xxs: "0.60rem",
+   //    xs: "0.75rem",
+   //    sm: "0.875rem",
+   //    md: "1rem",
+   //    lg: "1.125rem",
+   //    xl: "1.25rem",
+   //    "2xl": "1.5rem",
+   //    "3xl": "1.875rem",
+   //    "4xl": "2.25rem",
+   //    "5xl": "3rem",
+   //    "6xl": "3.75rem",
+   //    "7xl": "4.5rem",
+   //    "8xl": "6rem",
+   //    "9xl": "8rem",
+   //  },
+   //  fontWeights: {
+   //    hairline: 100,
+   //    thin: 200,
+   //    light: 300,
+   //    normal: 400,
+   //    medium: 500,
+   //    semibold: 600,
+   //    bold: 700,
+   //    extrabold: 800,
+   //    black: 900,
+   //  },
    styles: {
       global: {
          html: {
