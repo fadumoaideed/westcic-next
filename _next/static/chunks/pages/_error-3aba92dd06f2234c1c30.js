3f98c13bcbf770d2ca974a0bf4bc6fb8e18e2198
fadumoaideed <71390607+fadumoaideed@users.noreply.github.com>
2022-01-21 19:18:51 +0000

    Deploying to gh-pages from  @ 6a87c8c794369954a403c16ee7e0460ebb8626c4 🚀

--- _next/static/chunks/pages/_error-3aba92dd06f2234c1c30.js
+++
@@ -1,1 +0,0 @@
-_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[11],{"04ac":function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return o("/a9y")}])},TqRt:function(n,_){n.exports=function(n){return n&&n.__esModule?n:{default:n}}}},[["04ac",0,1,5]]]);
