7e4e41765c286f2cb414631831dff59bd3f632c8
fadumoaideed <fadumo.ahmed.aideed@gmail.com>
2022-06-20 17:12:58 +0100

    comment out services and readded package lock

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -1,57 +1,7 @@
-// import React from 'react'
-// import { Box } from '@chakra-ui/layout'
-// import { Parallax, ParallaxLayer } from '@react-spring/parallax'
-// function Services() {
-//    return (
-//       <Box h="400">
-//          <Parallax pages={2} style={{ top: '0', left: '0' }}>
-//             <ParallaxLayer
-//                offset={0}
-//                speed={2.5}
-//                style={{
-//                   display: 'flex',
-//                   justifyContent: 'center',
-//                   alignItems: 'center'
-//                }}
-//             >
-//                <p>Scroll down</p>
-//             </ParallaxLayer>
+import React from 'react'
 
-//             <ParallaxLayer
-//                offset={1}
-//                speed={2}
-//                style={{ backgroundColor: '#ff6d6d' }}
-//             />
+function services() {
+   return <div>services</div>
+}
 
-//             <ParallaxLayer
-//                offset={1}
-//                speed={0.5}
-//                style={{
-//                   display: 'flex',
-//                   justifyContent: 'center',
-//                   alignItems: 'center',
-//                   color: 'white',
-//                   backgroundColor: 'blue'
-//                }}
-//             >
-//                <p>Scroll up</p>
-//             </ParallaxLayer>
-//             <ParallaxLayer
-//                offset={1}
-//                speed={0.5}
-//                style={{
-//                   display: 'flex',
-//                   justifyContent: 'center',
-//                   alignItems: 'center',
-//                   color: 'white',
-//                   backgroundColor: 'pink'
-//                }}
-//             >
-//                <p>Scroll up</p>
-//             </ParallaxLayer>
-//          </Parallax>
-//       </Box>
-//    )
-// }
-
-// export default Services
+export default services
