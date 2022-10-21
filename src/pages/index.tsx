e850bd93586a39ef2d5fd6fb0d67e158489f2d22
fadumoaideed <fadumo.ahmed.aideed@gmail.com>
2022-10-21 15:32:37 +0100

    merging changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,16 @@ import Resources from 'components/Resources/Resources'
 import Footer from 'components/Footer/Footer'
 import Project from 'components/Rivet1/Rivet'
 
-// const prefix = '/westcic-next'
+//TODO: Work needed for MVP
+// - add hyphen between human and centered
+// - add email link (nodemailer form?) with auth
+// - add images and graphics
+// - find replacement for cloundinary
+// - reduce size of navbar in mobile mode and desktop (use Window.innerHeight)
+// - increase size of navbar header in mobile mode
+// - Reduce spacing between clean energy and africa
+// - change font, weight and background colour of navbar dropdown
+// - rename navbar to one page style
 
 export default function Home() {
    // const button = useStyleConfig('Button', { variant: 'small' })
